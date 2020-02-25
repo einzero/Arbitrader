814955fbd73d10ec2fb733383e01d56b26c6554e
Sunwung Jin <einzero@gmail.com>
2020-02-26 02:55:43 +0900

    - add base files - implement auto login and balance check

---
+++ src/MainForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Arbitrader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.OpenAPI = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label_Server = new System.Windows.Forms.Label();
+            this.label_UserId = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox_Account = new System.Windows.Forms.ComboBox();
+            this.dataGridView_Balance = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.OpenAPI)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Balance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenAPI
+            // 
+            this.OpenAPI.Enabled = true;
+            this.OpenAPI.Location = new System.Drawing.Point(12, 12);
+            this.OpenAPI.Name = "OpenAPI";
+            this.OpenAPI.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("OpenAPI.OcxState")));
+            this.OpenAPI.Size = new System.Drawing.Size(67, 17);
+            this.OpenAPI.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label_Server, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label_UserId, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox_Account, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 46);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(182, 111);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label_Server
+            // 
+            this.label_Server.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_Server.AutoSize = true;
+            this.label_Server.Location = new System.Drawing.Point(94, 77);
+            this.label_Server.Margin = new System.Windows.Forms.Padding(3);
+            this.label_Server.Name = "label_Server";
+            this.label_Server.Size = new System.Drawing.Size(85, 31);
+            this.label_Server.TabIndex = 4;
+            this.label_Server.Text = "label_Server";
+            this.label_Server.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_UserId
+            // 
+            this.label_UserId.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_UserId.AutoSize = true;
+            this.label_UserId.Location = new System.Drawing.Point(94, 40);
+            this.label_UserId.Margin = new System.Windows.Forms.Padding(3);
+            this.label_UserId.Name = "label_UserId";
+            this.label_UserId.Size = new System.Drawing.Size(85, 31);
+            this.label_UserId.TabIndex = 4;
+            this.label_UserId.Text = "label_Name";
+            this.label_UserId.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(3, 77);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "서버";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "계좌번호";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "아이디";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox_Account
+            // 
+            this.comboBox_Account.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox_Account.FormattingEnabled = true;
+            this.comboBox_Account.Location = new System.Drawing.Point(94, 3);
+            this.comboBox_Account.Name = "comboBox_Account";
+            this.comboBox_Account.Size = new System.Drawing.Size(85, 20);
+            this.comboBox_Account.TabIndex = 5;
+            // 
+            // dataGridView_Balance
+            // 
+            this.dataGridView_Balance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Balance.Location = new System.Drawing.Point(330, 183);
+            this.dataGridView_Balance.Name = "dataGridView_Balance";
+            this.dataGridView_Balance.RowTemplate.Height = 23;
+            this.dataGridView_Balance.Size = new System.Drawing.Size(458, 255);
+            this.dataGridView_Balance.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_Balance);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.OpenAPI);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Arbitrader";
+            ((System.ComponentModel.ISupportInitialize)(this.OpenAPI)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Balance)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI OpenAPI;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_Server;
+        private System.Windows.Forms.Label label_UserId;
+        private System.Windows.Forms.ComboBox comboBox_Account;
+        private System.Windows.Forms.DataGridView dataGridView_Balance;
+    }
+}
+
---
+++ src/MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Arbitrader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            OpenAPI.OnEventConnect += AxKHOpenAPI1_OnEventConnect;
+            OpenAPI.OnReceiveTrData += AxKHOpenAPI1_OnReceiveTrData;
+            OpenAPI.CommConnect();
+
+            _timer.Tick += Timer_Tick;
+            _timer.Interval = 200;
+            _timer.Start();
+
+            _beginTime = Time();
+            RegisterAction(5000, UpdateBalances);
+
+            dataGridView_Balance.Columns.Add("예수금", "예수금");
+            dataGridView_Balance.Columns.Add("D+2추정예수금", "D+2추정예수금");
+            dataGridView_Balance.Columns.Add("유가잔고평가액", "유가잔고평가액");
+            dataGridView_Balance.Columns.Add("예탁자산평가액", "예탁자산평가액");
+            dataGridView_Balance.Columns.Add("총매입금액", "총매입금액");
+            dataGridView_Balance.Rows.Add();
+        }
+   
+        private void AxKHOpenAPI1_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode != 0)
+            {
+                Application.Exit();
+            }
+
+            _loginSucceed = true;
+
+            string acclist = OpenAPI.GetLoginInfo("ACCLIST");
+            string[] accounts = acclist.Trim().Split(';').Where(x => x.Length > 0).ToArray();
+            comboBox_Account.Items.AddRange(accounts);
+            comboBox_Account.SelectedIndex = 0;
+
+            label_UserId.Text = OpenAPI.GetLoginInfo("USER_ID");
+            label_Server.Text = OpenAPI.GetLoginInfo("GetServerGubun") == "1" ? "모의 투자" : "실서버";
+
+            Show();
+        }
+
+        private void AxKHOpenAPI1_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            string 예수금 = GetTrData(e, "예수금");
+            string D2추정예수금 = GetTrData(e, "D+2추정예수금");
+            string 유가잔고평가액 = GetTrData(e, "유가잔고평가액");
+            string 예탁자산평가액 = GetTrData(e, "예탁자산평가액");
+            string 총매입금액 = GetTrData(e, "총매입금액");
+
+            var row = dataGridView_Balance.Rows[0];
+            row.SetValues(예수금, D2추정예수금, 유가잔고평가액, 예탁자산평가액, 총매입금액);
+        }
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(CheckDisconnected())
+            {
+                _timer.Stop();
+                MessageBox.Show("접속이 종료되었습니다.\n 프로그램을 종료합니다.");
+                Application.Exit();
+                return;
+            }
+
+            DateTime now = Time();
+            foreach(var item in _actions)
+            {
+                TimeSpan span = now - item.LastTime;
+                if(span.TotalMilliseconds >= item.Interval)
+                {
+                    item.LastTime = now;
+                    item.Action();
+                }
+            }
+        }
+
+        private bool CheckDisconnected()
+        {
+            TimeSpan span = TimeElapsed();
+            bool notConnectedYet = span.TotalSeconds > 20 && !_loginSucceed;
+            bool disconnected = _loginSucceed && !IsConnected();
+            return notConnectedYet || disconnected;
+        }
+
+        private void UpdateBalances()
+        {
+            SetInputValue("계좌번호", GetAccount());
+            SetInputValue("비밀번호", "");
+            SetInputValue("상장폐지조회구분", "1");
+            SetInputValue("비밀번호입력매체구분", "00");
+            CommRqData("OPW00004");
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            base.OnVisibleChanged(e);
+            Visible = IsConnected();
+        }
+
+        private string GetAccount()
+        {
+            return (string)comboBox_Account.SelectedItem;
+        }
+
+        private bool IsConnected()
+        {
+            return OpenAPI.GetConnectState() == 1;
+        }
+
+        private TimeSpan TimeElapsed()
+        {
+            return Time() - _beginTime;
+        }
+
+        private DateTime Time()
+        {
+            return DateTime.Now;
+        }
+
+        private void SetInputValue(string sID, string sValue)
+        {
+            OpenAPI.SetInputValue(sID, sValue);
+        }
+
+        private void CommRqData(string sTrCode)
+        {
+            OpenAPI.CommRqData("RQName", sTrCode, 0, "화면번호");
+        }
+
+        private string GetTrData(AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e, string sName)
+        {
+            var data = OpenAPI.GetCommData(e.sTrCode, e.sRecordName, 0, sName);
+            return data.Trim();
+        }
+
+        private void RegisterAction(int interval, Action action)
+        {
+            var item = new TimerAction();
+            item.Interval = interval;
+            item.Action = action;
+            item.LastTime = Time();
+            _actions.Add(item);
+        }
+
+        private bool _loginSucceed;
+        private Timer _timer = new Timer();
+        private DateTime _beginTime;
+        private List<TimerAction> _actions = new List<TimerAction>();
+
+        private class TimerAction
+        {
+            // in ms
+            public int Interval;
+            public Action Action;
+
+            public DateTime LastTime;
+        }
+    }    
+}
+  
