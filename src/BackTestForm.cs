c1cb3077c57d4bfdac20e3e0ed9f4a1463f0f2d3
Sunwung Jin <einzero@gmail.com>
2020-03-26 02:43:41 +0900

    Implement basic arbitrage trading
    - Implemented basic arbitrage trading based on the end price.

--- src/BackTestForm.cs
+++ src/BackTestForm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Arbitrader
@@ -44,15 +43,57 @@ namespace Arbitrader
 
         private async void button_Test_Click(object sender, EventArgs e)
         {
-            var stock = comboBox_종목1.SelectedItem as OpenApi.Stock;
-            var date = dateTimePicker_Begin.Value;
+            var stock1 = comboBox_종목1.SelectedItem as OpenApi.Stock;
+            if(stock1 == null)
+            {
+                return;
+            }
 
-            var day = date.ToString("yyyyMMdd");
-            var result = await StockPriceCollection.Get(stock.Code, day);
+            var collection1 = await StockPriceCollection.Get(stock1.Code, dateTimePicker_Begin.Value, dateTimePicker_End.Value);
+            Debug.Info("{0}", collection1.Items.Count);
 
-            Debug.Info("{0}", result.Items.Count);
+            var stock2 = comboBox_종목2.SelectedItem as OpenApi.Stock;
+            if (stock2 == null)
+            {
+                return;
+            }
+            var collection2 = await StockPriceCollection.Get(stock2.Code, dateTimePicker_Begin.Value, dateTimePicker_End.Value);
 
-            int x = 5;
+            Debug.Info("{0}", collection2.Items.Count);
+
+            var items1 = collection1.Items;
+            var items2 = collection2.Items;
+
+
+            bool isItem1 = items1[0].Price <= items2[0].Price;
+            const long quantity = 2000;
+
+            long sum = 0;
+            for(int i = 1; i < items1.Count; ++i)
+            {
+                var price1 = items1[i].Price;
+                var price2 = items2[i].Price;
+
+                var mine = isItem1 ? price1 : price2;
+                var other = isItem1 ? price2 : price1;
+
+                var mineValue = mine * quantity;
+                var otherValue = other * quantity;
+
+                if (otherValue <= mineValue - mine)
+                {
+                    long diff = mineValue - otherValue;
+                    long count = diff / mine;
+                    long profit = count * mine;
+                    sum += profit;
+
+                    isItem1 = !isItem1;
+
+                    Debug.Info("Date: {0} - Item1: {1}, Item2: {2}, Profit: {3}", items1[i].Date, price1, price2, profit);
+                }
+            }
+
+            Debug.Info("Total Profit: {0}", sum);
         }
     }
 }
--- src/StockPrice.cs
+++ src/StockPrice.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,45 +8,49 @@ namespace Arbitrader
 {
     public class StockPrice
     {
-        public string Date;
-        public int StartPrice;
-        public int HighPrice;
-        public int LowPrice;
-        public int Price;
+        public DateTime Date;
+        public long StartPrice;
+        public long HighPrice;
+        public long LowPrice;
+        public long Price;
     }
 
     public class StockPriceCollection
     {
         private TaskCompletionSource<StockPriceCollection> _source = new TaskCompletionSource<StockPriceCollection>();
         private string _code;
-        private string _date;
+        private DateTime _begin;
+        private DateTime _end;
 
         public List<StockPrice> Items = new List<StockPrice>();
 
-        public static async Task<StockPriceCollection> Get(string code, string date)
+        public static Task<StockPriceCollection> Get(string code, DateTime begin, DateTime end)
         {
-            var collection = new StockPriceCollection();
-            await Task.Factory.StartNew(delegate
-            {
-                collection.Request(code, date, collection);
-            }, TaskCreationOptions.AttachedToParent);
-               
-            return await Task.FromResult(collection._source.Task.Result);
+            var collection = new StockPriceCollection();            
+            collection.Request(code, 
+                new DateTime(begin.Year, begin.Month, begin.Day), 
+                new DateTime(end.Year, end.Month, end.Day), 
+                collection);
+
+            return collection._source.Task;
         }
 
-        private void Request(string code, string date, StockPriceCollection collection, int seq = 0)
+        private void Request(string code, DateTime begin, DateTime end, StockPriceCollection collection, int seq = 0)
         {
             _code = code;
-            _date = date;
+            _begin = begin;
+            _end = end;
 
             OpenApi.SetInputValue("종목코드", code);
-            OpenApi.SetInputValue("기준일자", date);
+            OpenApi.SetInputValue("기준일자", end.ToString("yyyyMMdd"));
             OpenApi.SetInputValue("수정주가구분", "0");
             OpenApi.CommRqData("opt10081", collection.PriceCallback, seq);
         }
 
         private void PriceCallback(AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
         {
+            bool continued = true;
+
             int count = OpenApi.GetRepeatCnt(e);
             for(int i = 0; i < count; ++i)
             {
@@ -55,26 +61,32 @@ namespace Arbitrader
                 string price = OpenApi.GetTrData(e, "현재가", i);
 
                 var stock = new StockPrice();
-                stock.Date = date;
-                int.TryParse(startPrice, out stock.StartPrice);
-                int.TryParse(highPrice, out stock.HighPrice);
-                int.TryParse(lowPrice, out stock.LowPrice);
-                int.TryParse(price, out stock.Price);
+
+                DateTime.TryParseExact(date, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out stock.Date);
+                long.TryParse(startPrice, out stock.StartPrice);
+                long.TryParse(highPrice, out stock.HighPrice);
+                long.TryParse(lowPrice, out stock.LowPrice);
+                long.TryParse(price, out stock.Price);
+
+                if(stock.Date < _begin)
+                {
+                    continued = false;
+                    break;
+                }
 
                 Items.Add(stock);
             }
 
-            Debug.Info("Price Received({0}): {1}", _code, Items.Count);
-
             int seq;
             int.TryParse(e.sPrevNext, out seq);
-            if (seq != 0)
+            if (seq != 0 && continued)
             {
                 Thread.Sleep(300);
-                Request(_code, _date, this, seq);
+                Request(_code, _begin, _end, this, seq);
             }
             else
             {
+                Items.Reverse();
                 _source.SetResult(this);
             }
         }
